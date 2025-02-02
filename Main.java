d4dc20589377dcad8b42134abd4b48a88c62221c
Anusri Kadam <anusrikadam@gmail.com>
2025-02-02 16:47:24 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,5 +6,17 @@ class Main{
 		int[] array = ui.arrayInput();
 		functions.evenOdd(array);
 		functions.difference(array);
+
+		 // Convert array to ArrayList and display
+	        System.out.println("\nConverting Array to ArrayList:");
+	        System.out.println(functions.arrayToArrayList(array));
+	
+	        // Convert ArrayList back to array and display
+	        System.out.println("\nConverting ArrayList back to Array:");
+	        int[] newArray = functions.arrayListToArray(functions.arrayToArrayList(array));
+	        for (int num : newArray) {
+	            System.out.print(num + " ");
+	        }
+	        System.out.println();	
 	}
 }
