c7d99ca3135eee8afa7bdd6249fddad1e7d58a1d
Anusri Kadam <anusrikadam@gmail.com>
2025-02-02 17:04:05 +0530

    Update UserInput.java

--- UserInput.java
+++ UserInput.java
@@ -2,15 +2,15 @@
 import java.util.*;
 class UserInput{
 		int[] arrayInput(){
+			// Accepts 5 integers from the user and returns them as an array
 			int[] array = new int[5];
 			Scanner scan = new Scanner(System.in);
-			
+			//prompts user for 5 numbers
 			System.out.println("Enter 5 numbers");
 			for(int i = 0; i< array.length; i++){
 				array[i] = scan.nextInt();
 			}
-			scan.close();
-			return array;
-			
+			scan.close();//Closing the scanner 
+			return array; // Return the filled array
 		}	
 	}
